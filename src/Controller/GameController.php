189483290b6780cbfb86701670731cd76e0e4264
zemac <etienne.ziegelmeyer@lapiscine.pro>
2019-08-25 18:14:55 +0200

    add form create game dont make persist

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use App\Form\GameType;
 use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +22,24 @@ class GameController extends AbstractController
 
             ]);
     }
+
+    //    CRee un nouvelle article Game
+
+    /**
+     * @Route("/games/new", name="game_create")
+     */
+    public function create()
+    {
+        $form = $this->createForm(GameType::class);
+
+        return $this->render('game/create_game.html.twig',
+            [
+                'gameForm' => $form->createView()
+            ]);
+    }
+
+
+
     /**
      * @Route("/games/{id}", name="games_show")
      */
@@ -32,4 +52,5 @@ class GameController extends AbstractController
             ]);
 
     }
+
 }

8b8c8583ce9fcb0fafd46e703324bc00d86efa62
zemac <etienne.ziegelmeyer@lapiscine.pro>
2019-08-25 21:16:18 +0200

    add function and test  persist to db and work

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Game;
 use App\Form\GameType;
 use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GameController extends AbstractController
@@ -23,14 +25,33 @@ class GameController extends AbstractController
             ]);
     }
 
-    //    CRee un nouvelle article Game
+
+// ---------------------Cree un nouveux Game en DB-----------------
 
     /**
      * @Route("/games/new", name="game_create")
      */
-    public function create()
+    public function create(Request $request, EntityManagerInterface $entityManager)
     {
         $form = $this->createForm(GameType::class);
+//        j 'instancie un l entite Game
+        $game = new Game();
+
+//       J assigne a $form la creation d'un formumaire et je lui lie mon instance de class Game($game)
+        $form = $this->createForm(GameType::class, $game);
+
+//        je recupere les donnee de mon formulaire et j annalise la requet .Si et seloemnt si la requet est de type Post
+        $form->handleRequest($request);
+        //dump($game); Je verifie si la requet est remplie de donnees.
+
+        //Si le form et envoyer et valider
+        if ($form->isSubmitted() && $form->isValid()) {
+            //On envoie le game en DB grace a permit et flush
+            $entityManager->persist($game);
+            $entityManager->flush();
+        // return a la page de tous les jeux.
+            return $this->render('game/games.html.twig');
+        }
 
         return $this->render('game/create_game.html.twig',
             [
@@ -38,7 +59,7 @@ class GameController extends AbstractController
             ]);
     }
 
-
+//------------------------------------------------
 
     /**
      * @Route("/games/{id}", name="games_show")
--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -4,9 +4,6 @@ namespace App\Form;
 
 use App\Entity\Game;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,18 +12,8 @@ class GameType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class,[
-                'attr' => [
-                    'placeholder' => "Nom du jeux"
-
-                ]
-            ])
-            ->add('description', TextareaType::class,[
-                'attr' => [
-                    'placeholder' => "Description du jeux"
-
-                ]
-            ])
+            ->add('title')
+            ->add('description')
             ->add('picture')
 
         ;
